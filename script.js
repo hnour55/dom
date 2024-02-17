cb0d76a9b0a0dd9ec8f65d4cb64ad9998b0bced8
harrabi nour houda <nharrabi9@gmail.com>
2024-02-17 12:32:50 +0100

    shopping card

---
+++ script.js
@@ -0,0 +1,52 @@
+﻿var btnplus=Array.from(document.getElementsByClassName('btn+'))
+console.log('btn+' ,btnplus);
+//btnplus 
+for(let el of btnplus){
+    el.addEventListener("click",function(){
+        let t=(el.previousElementSibling).value;
+        let s=parseInt(t);
+        s++;
+        (el.previousElementSibling).value=s;
+        sum();
+    })
+}
+//btnmoins 
+var btnmoin=Array.from(document.getElementsByClassName('btn-'))
+for(let el of btnmoin){
+    el.addEventListener("click",function(){
+        let t=(el.nextElementSibling).value;
+        let s=parseInt(t);
+        if(s>0){
+            s--;
+        }
+        (el.nextElementSibling).value=s;
+        sum();
+    })
+}
+
+// function sum
+function sum(){
+    var price=Array.from(document.getElementsByClassName('price'))
+    var qtn=Array.from(document.getElementsByClassName('qtn'))
+    var s=0
+    for (let i = 0; i<price.length; i++) {
+        s+=parseInt(price[i].innerText)*parseInt(qtn[i].value)
+    }
+    return (document.querySelector(".total").innerHTML=s+" "+"TND")
+}
+
+//function heart 
+var heart=Array.from(document.getElementsByClassName('fa-heart'))
+for(let el of heart){
+    el.addEventListener("click",function(){
+        el.classList.toggle("red")
+    })
+}
+
+var dl=Array.from(document.getElementsByClassName('fa-trash'))
+
+for(let el of dl){
+    el.addEventListener("click",function(){
+        el.parentNode.remove();;
+    })
+}
